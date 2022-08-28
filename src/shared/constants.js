b37e844fe04773549a8f79d21b73ddd4599d4997
ssnsankar <ssn.narayanan@gmail.com>
2022-08-28 10:58:34 +0530

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,4 +1,5 @@
-const baseUrl = 'https://cdeb397r4l.execute-api.ap-south-1.amazonaws.com/api'// 'http://localhost:52100/api'
+const baseUrl = 'https://0nrae2fee9.execute-api.ap-south-1.amazonaws.com/Dev'
+                    //'https://cdeb397r4l.execute-api.ap-south-1.amazonaws.com/api' - 'http://localhost:52100/api'
 const apiName = 'skill-tracker'
 const api = 'api'
 const version = 'v1'
